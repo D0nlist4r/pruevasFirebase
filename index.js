d095870a6529b9a2c460f2948e8bf7e00f4bfcd3
Sebastian <dev_sebasfc@outlook.es>
2022-11-21 18:09:04 -0500

    ver datos tiempo real

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-window.addEventListener('DOMContentLoaded',()=>{
+import {Tasksave, getTasks, ongetTask} from './firebase.js';
 
-});
 const taskform = document.getElementById('task-form');
-import {Tasksave} from './firebase.js'
+const taskContainer = document.getElementById('tasks-container');
+let html = ``;
+
+window.addEventListener('DOMContentLoaded', async () => {
+  
+  ongetTask(querysnapshot => {
+  querysnapshot.forEach(doc => {
+    const task = doc.data();
+    html+=`
+    <div>
+    <h3>${task.title}</h3>
+    <p>${task.description}</p>
+    </div>`
+  } );
+  taskContainer.innerHTML= html;
+});
+});
+
+
 
 taskform.addEventListener('submit',(e)=>{
   e.preventDefault()
   const title = taskform['task-title'];
   const description = taskform['task-description'];
 
-  Tasksave(title.value,description.value);
+  Tasksave(title.value,description.value); //creacion de un nuevo objeto en la database
+
+  taskform.reset();
 })
