ba303edf0898d195398cf2671e5d34d422aa193b
Sebastian <dev_sebasfc@outlook.es>
2022-11-23 23:44:55 -0500

    Termiando logica de firebase

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-  import {getFirestore,collection,addDoc,getDocs,onSnapshot , deleteDoc, doc ,} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
+  import {getFirestore,collection,addDoc,getDocs,onSnapshot , deleteDoc, doc , getDoc, updateDoc} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -25,3 +25,7 @@
   export const getTasks = ()=> getDocs(collection(db,'tasks'));
   export const ongetTask = (callback) => onSnapshot(collection(db,'tasks'),callback);
   export const deleteTask = (id) => deleteDoc(doc(db,'tasks',id));
+  export const getTask = (id) => getDoc(doc(db,'tasks',id));
+  export const updateTask = (id,newfiles) => {
+    updateDoc(doc(db,'tasks',id),newfiles)
+  }
--- index.js
+++ index.js
@@ -1,29 +1,49 @@
-import {Tasksave, getTasks, ongetTask , deleteTask} from './firebase.js';
+import {Tasksave, getTasks, ongetTask , deleteTask, getTask, updateTask} from './firebase.js';
 
 const taskform = document.getElementById('task-form');
 const taskContainer = document.getElementById('tasks-container');
+let editStatus = false;
+let id = "";
 let html = ``;
 
 window.addEventListener('DOMContentLoaded', async () => {
   
   ongetTask(querysnapshot => {
+
+    taskContainer.innerHTML = "";
+
   querysnapshot.forEach(doc => {
     const task = doc.data();
-    html+=`
+    taskContainer.innerHTML +=`
     <div>
     <h3>${task.title}</h3>
     <p>${task.description}</p>
     <button class="btn-delete" data-id="${doc.id}">Delete</button>
+    <button class="btn-edit" data-id="${doc.id}">Edit</button>
     </div>`
      } );
-    taskContainer.innerHTML= html;
+
     const btnTaskdelete = taskContainer.querySelectorAll('.btn-delete');
-    btnTaskdelete.forEach( btn =>{
-    btn.addEventListener("click", ({ target: { dataset } })=>{
-       deleteTask(dataset.id);
-    })
-  })
- });
+        btnTaskdelete.forEach( btn => {
+          btn.addEventListener("click", ({ target: { dataset } })=>{
+            deleteTask(dataset.id); // mandando id para eliminar
+          })
+       });
+
+    const btnTaskedit = taskContainer.querySelectorAll('.btn-edit');
+    btnTaskedit.forEach((btn) => {
+      btn.addEventListener("click",async (e) => {
+        const doc = await getTask(e.target.dataset.id);
+        const task = doc.data();
+        taskform['task-title'].value = task.title;
+        taskform['task-description'].value = task.description;
+
+        editStatus = true;
+        id = doc.id;
+        taskform['btn-task-save'].innerText = 'update';
+      });
+    })    
+  });
 });
 
 
@@ -32,8 +52,14 @@ taskform.addEventListener('submit',(e)=>{
   e.preventDefault()
   const title = taskform['task-title'];
   const description = taskform['task-description'];
-
-  Tasksave(title.value,description.value); //creacion de un nuevo objeto en la database
+  if(!editStatus){
+    Tasksave(title.value,description.value); //creacion de un nuevo objeto en la database
+  }else{
+    updateTask(id,{title: title.value,description: description.value})
+    editStatus = false;
+    id = "";
+    taskform['btn-task-save'].innerText = 'save';
+  }
 
   taskform.reset();
 })
